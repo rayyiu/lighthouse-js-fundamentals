889cfe9aa41b4572978df5b3aff640bd57b03a4b
Ray Yiu <rayyiu@pop-os.localdomain>
2021-01-11 17:29:41 -0800

    more conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,5 @@
 const temperature = 0
+const raining = false
 
 if(temperature < 0) {
     console.log("Make sure you pick out a scarf!")
@@ -10,4 +11,21 @@ if(temperature < 0) {
 
 
 console.log("Now you're ready to go outside!");
+
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+}
+
 
